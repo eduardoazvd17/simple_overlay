eba09f707f5765d80ac1414751e26a4d1a2dbaa1
Eduardo Azevedo <eduardoazvd17@gmail.com>
2022-10-28 20:11:41 -0300

    Building widget base using overlay api

--- lib/widgets/simple_overlay_widget.dart
+++ lib/widgets/simple_overlay_widget.dart
@@ -1,11 +1,67 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dart:developer' as developer;
+
+import 'package:flutter/material.dart';
 
 class SimpleOverlayWidget extends StatelessWidget {
-  const SimpleOverlayWidget({super.key});
+  final String tag;
+  final Widget child;
+  late final OverlayState _overlayState;
+  late final OverlayEntry _overlayEntry;
+  final void Function()? onShow;
+  final void Function()? onHide;
+
+  SimpleOverlayWidget({
+    required this.tag,
+    required BuildContext context,
+    required Widget overlay,
+    required this.child,
+    void Function()? onTapOutside,
+    this.onShow,
+    this.onHide,
+    double? top,
+    double? left,
+    double? bottom,
+    double? right,
+  }) : super(key: Key(tag)) {
+    _overlayState = Overlay.of(context)!;
+    _overlayEntry = OverlayEntry(builder: (contextBuilder) {
+      //final renderBox = context.findRenderObject() as RenderBox;
+      return Stack(
+        children: [
+          if (onTapOutside != null) GestureDetector(onTap: () => hide()),
+          Positioned(
+            top: top,
+            left: left,
+            bottom: bottom,
+            right: right,
+            child: overlay,
+          ),
+        ],
+      );
+    });
+    show();
+  }
+
+  void show() {
+    try {
+      Future.delayed(const Duration(milliseconds: 200)).then((value) {
+        _overlayState.insert(_overlayEntry);
+        onShow?.call();
+      });
+    } catch (_) {
+      developer.log("Error on show overlay $tag", name: "simple_overlay");
+    }
+  }
+
+  void hide() {
+    try {
+      _overlayEntry.remove();
+      onHide?.call();
+    } catch (_) {
+      developer.log("Error on hide overlay $tag", name: "simple_overlay");
+    }
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  Widget build(BuildContext context) => child;
 }

9ef345f860238be3b1ab2ab42579149a330d10fd
Eduardo Azevedo <eduardoazvd17@gmail.com>
2022-10-28 20:10:49 -0300

    Innitial commit

---
+++ lib/simple_overlay.dart
@@ -0,0 +1,1 @@
+export 'widgets/simple_overlay_widget.dart';
---
+++ lib/widgets/simple_overlay_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SimpleOverlayWidget extends StatelessWidget {
+  const SimpleOverlayWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

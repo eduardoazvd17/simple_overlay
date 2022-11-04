9556ee64a28019d5636ebc46c7b4953053c50437
Eduardo Azevedo <eduardoazvd17@gmail.com>
2022-11-04 18:02:24 -0300

    Changing usage to a method instead of constructor

--- lib/simple_overlay.dart
+++ lib/simple_overlay.dart
@@ -3,73 +3,90 @@ import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
 
 class SimpleOverlay {
-  final BuildContext context;
-  final Widget overlay;
-  final bool hideOnTapOutside;
-
-  final void Function()? onShow;
-  final void Function()? onHide;
-  final void Function()? onTapOutside;
-
-  late final OverlayState _overlayState;
-  late final OverlayEntry _overlayEntry;
+  OverlayState? _overlayState;
+  OverlayEntry? _overlayEntry;
+  Function()? onHide;
 
   bool _isVisible = false;
   bool get isVisible => _isVisible;
 
-  SimpleOverlay({
-    required this.context,
-    required this.overlay,
-    this.hideOnTapOutside = true,
-    this.onShow,
-    this.onHide,
-    this.onTapOutside,
-    bool startShowing = false,
+  void show({
+    required Widget overlay,
+    required BuildContext context,
+    bool hideOnTapOutside = true,
     double? top,
     double? left,
     double? bottom,
     double? right,
+    Function()? onShow,
+    Function()? onHide,
+    Function()? onTapOutside,
+    Duration? delay,
   }) {
-    _overlayState = Overlay.of(context)!;
-    _overlayEntry = OverlayEntry(builder: (contextBuilder) {
-      //final renderBox = context.findRenderObject() as RenderBox;
-      return Stack(
-        children: [
-          GestureDetector(
-            onTap: hideOnTapOutside ? () => hide() : null,
-          ),
-          Positioned(
-            top: top,
-            left: left,
-            bottom: bottom,
-            right: right,
-            child: overlay,
-          ),
-        ],
-      );
-    });
-    if (startShowing) show();
-  }
-
-  void show() {
+    this.onHide = onHide;
     try {
-      _isVisible = true;
-      Future.delayed(const Duration(milliseconds: 200)).then((value) {
-        _overlayState.insert(_overlayEntry);
-        onShow?.call();
+      _hideOverlay(log: false);
+      _overlayState = Overlay.of(context)!;
+      _overlayEntry = OverlayEntry(builder: (contextBuilder) {
+        //final renderBox = context.findRenderObject() as RenderBox;
+        return Stack(
+          children: [
+            GestureDetector(
+              onTap: hideOnTapOutside ? () => hide() : null,
+            ),
+            Positioned(
+              top: top,
+              left: left,
+              bottom: bottom,
+              right: right,
+              child: overlay,
+            ),
+          ],
+        );
       });
+
+      if (delay != null) {
+        Future.delayed(delay).then((_) {
+          _showOverlay();
+          onShow?.call();
+        });
+      } else {
+        _showOverlay();
+        onShow?.call();
+      }
     } catch (_) {
       developer.log("Error on show overlay", name: "simple_overlay");
     }
   }
 
   void hide() {
+    _hideOverlay();
+    onHide?.call();
+  }
+
+  void _showOverlay({bool log = true}) {
+    try {
+      _isVisible = true;
+      if (_overlayState != null && _overlayEntry != null) {
+        _overlayState!.insert(_overlayEntry!);
+      }
+
+      if (log) developer.log("Showing overlay", name: "simple_overlay");
+    } catch (_) {
+      if (log) developer.log("Error on show overlay", name: "simple_overlay");
+    }
+  }
+
+  void _hideOverlay({bool log = true}) {
     try {
       _isVisible = false;
-      _overlayEntry.remove();
-      onHide?.call();
-    } catch (e) {
-      developer.log("Error on hide overlay", name: "simple_overlay");
+      if (_overlayEntry != null) {
+        _overlayEntry!.remove();
+      }
+
+      if (log) developer.log("Hiding overlay", name: "simple_overlay");
+    } catch (_) {
+      if (log) developer.log("Error on hide overlay", name: "simple_overlay");
     }
   }
 }
